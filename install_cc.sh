13fb1d449e58a582271e42dd50c5ab6d7d386c93
learnin9 <learnin9@gmail.com>
2016-01-04 18:35:14 +0800

    Update install_cc.sh

--- install_cc.sh
+++ install_cc.sh
@@ -58,6 +58,71 @@ function php_install(){
 	sed -i "s/group = www-data/group = asterisk/" /etc/php5/fpm/pool.d/www.conf
 	sed -i "s/listen = \/var\/run\/php5-fpm.sock/listen = 127.0.0.1:9000/" /etc/php5/fpm/pool.d/www.conf
 	update-rc.d php5-fpm defaults
+cat /etc/init.d/php5-frpm <<EOF
+#!/bin/bash
+#
+# Startup script for the PHP-FPM server.
+#
+# chkconfig: 345 85 15
+# description: PHP is an HTML-embedded scripting language
+# processname: php-fpm
+# config: /usr/local/php/etc/php.ini
+ 
+# Source function library.
+. /lib/lsb/init-functions
+PHP_PATH=/usr/sbin
+DESC="php-fpm daemon"
+NAME=php5-fpm
+DAEMON=$PHP_PATH/$NAME
+CONFIGFILE=/etc/php5/fpm/php-fpm.conf
+PIDFILE=/var/run/$NAME.pid
+SCRIPTNAME=/etc/init.d/$NAME
+ 
+# Gracefully exit if the package has been removed.
+test -x $DAEMON || exit 0
+ 
+rh_start() {
+  $DAEMON -y $CONFIGFILE || echo -n " already running"
+}
+ 
+rh_stop() {
+  kill -QUIT `cat $PIDFILE` || echo -n " not running"
+}
+ 
+rh_reload() {
+  kill -HUP `cat $PIDFILE` || echo -n " can't reload"
+}
+ 
+case "$1" in
+  start)
+        echo -n "Starting $DESC: $NAME"
+        rh_start
+        echo "."
+        ;;
+  stop)
+        echo -n "Stopping $DESC: $NAME"
+        rh_stop
+        echo "."
+        ;;
+  reload)
+        echo -n "Reloading $DESC configuration..."
+        rh_reload
+        echo "reloaded."
+  ;;
+  restart)
+        echo -n "Restarting $DESC: $NAME"
+        rh_stop
+        sleep 1
+        rh_start
+        echo "."
+        ;;
+  *)
+         echo "Usage: $SCRIPTNAME {start|stop|restart|reload}" >&2
+         exit 3
+        ;;
+esac
+exit 0
+EOF
 	echo -e "\e[32mPHP-Fpm Install OK!\e[m"
 }
 
